13da387277b4051fc7acf9d330c8a5e7dda4d96a
Mohammad-Reza-Tagipour <mohammadrezatagipour45@gmail.com>
2023-11-29 17:14:10 +0330

    added fox also Contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello I'm
+        <span className="blue-gradient_text font-semibold drop-shadow ">
+          Mohammad reza
+        </span>
+      </h1>
+      <div>
+        <p className="mt-5 flex flex-col gap-3 text-slate-500">
+          👋 Front-end developer specializing in creating captivating user
+          interfaces and seamless web experiences
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text flex-wrap gap-12">My Skills</h3>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,19 @@
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import Fox from '../models/Fox.jsx';
+import Loader from '../components/Loader.jsx';
+import useAlert from '../hooks/useAlert.js';
+import Alert from '../components/Alert.jsx';
+// import { Loader } from '@react-three/drei';
 const Contact = ({}) => {
   const formRef = useRef(null);
-
   const [form, setForm] = useState({ name: '', email: '', message: '' });
-
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setcurrentAnimation] = useState('idle');
+
+  const { alert, showAlert, hideAlert } = useAlert();
+
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -13,6 +21,7 @@ const Contact = ({}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setcurrentAnimation('hit');
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -28,26 +37,43 @@ const Contact = ({}) => {
       )
       .then(() => {
         setIsLoading(false);
-        //Show success message
 
+        //Show success message
+        showAlert({
+          show: true,
+          text: 'Message sent succesfully',
+          type: 'success',
+        });
         //Hide an alert
 
-        setForm({ name: '', email: '', message: '' });
+        setTimeout(() => {
+          hideAlert();
+          setcurrentAnimation('idle');
+          setForm({ name: '', email: '', message: '' });
+        }, [3000]);
       })
       .catch((err) => {
         setIsLoading(false);
+        setcurrentAnimation('idle');
         console.log(err);
 
         // Show error message
+        showAlert({
+          show: true,
+          text: 'I didnt receive your message ',
+          type: 'danger',
+        });
       });
   };
   //once you click on it
-  const handleFocus = () => {};
+  const handleFocus = () => setcurrentAnimation('walk');
   // once you click out
-  const handleBlur = () => {};
+  const handleBlur = () => setcurrentAnimation('idle');
 
   return (
     <section className="relative flex  lg:flex-row  flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%]  flex flex-col">
         <h1 className="head-text "> Get in touch</h1>
         <form
@@ -106,6 +132,22 @@ const Contact = ({}) => {
             {isLoading ? 'Sending...' : 'Send message'}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2  w-full lg:h-auto md:h-[550px] h-[350px]">
+        {/* fov: feel of view */}
+        <Canvas camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
+          <directionalLight intensity={2.5} position={(0, 0, 1)} />
+          <ambientLight intensity={1} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
